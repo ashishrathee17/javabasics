7b36d906eeccae5677331eb65263d8cdd05c9938
Ashish Rathee <rathee.ashish@outlook.com>
2016-10-15 22:55:13 +0530

    Example of Blank Final Variable

---
+++ FinalVariable.java
@@ -0,0 +1,13 @@
+
+public class FinalVariable {
+	
+	   //static blank final variable  
+	   static final double PIE_VALUE;
+	   static{ 
+		   PIE_VALUE = 3.14;
+	   } 
+	
+	public static void main(String args[]) {
+		System.out.println("Value of Pie: "+PIE_VALUE);
+	} 	
+}
